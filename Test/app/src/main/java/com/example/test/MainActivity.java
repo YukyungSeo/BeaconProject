36d5b587c20ab50a2ac8573189699496d4415ba1
ju8ha <mukpobu@gmail.com>
2020-07-16 15:12:52 +0900

    add test file

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    TextView id_text;
+    Button firstButton;
+    Button secondButton;
+    TextView BLEDataText;
+    //추가로 변수가 필요할 경우 단톡방에 꼭 말해주세요.
+    //push 할때도 꼭 단톡방에 말해주세요.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstButton = (Button)findViewById(R.id.firstButton);
+        secondButton = (Button)findViewById(R.id.secondButton);
+        BLEDataText = (TextView) findViewById(R.id.BLEDataText);
+
+        firstButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                id_text= (TextView) findViewById(R.id.idInputText);  //입력받은 id 값
+                //1단계 부분입니다.
+            }
+        });
+
+        secondButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                id_text= (TextView) findViewById(R.id.idInputText);  //입력받은 id 값
+                //2단계 부분입니다.
+                //BLEDataText에 같은 영역의 BLE를 출력하면 됩니다.
+            }
+        });
+
+
+    }
+}
