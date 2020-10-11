41fd657bfde2bc9d541dfaf6817a7d128ea26d52
YukyungSeo <syk332@naver.com>
2020-10-12 02:17:00 +0900

    Correct code overall frame

--- bServer/src/Correction/CorrectTest.java
+++ bServer/src/Correction/CorrectTest.java
@@ -2,33 +2,23 @@ package Correction;
 
 import java.io.IOException;
 import beaconLocation.ComputeBeaconTest;
-import bleDistance.ComputeTester;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import pack.ComputeTester;
 
 public class CorrectTest {
     static String TESTMODE_BENCHSET="benchsettest";
     static String TESTMODE_TXT="txttest";
 
-    public static void main (String args[]) throws IOException {
-    	
-    	ComputeBeaconTest beaconTester = new ComputeBeaconTest();
-    	beaconTester.test();
-    	ArrayList<Phone> phoneList = beaconTester.getPhoneArr();	//use this
-    	
+    public static void main (String args[]) throws IOException, ClassNotFoundException {
+        ComputeBeaconTest beaconTester = new ComputeBeaconTest();
+        beaconTester.test();
+        ArrayList<Phone> phoneList = beaconTester.getPhoneArr();	//use this
+
         ComputeTester tester= new ComputeTester();
-        tester.startTest(TESTMODE_BENCHSET,".\temp_bleResult.csv");
-        Correct correct = new Correct(2, 2, 9, 9, fillList(),tester.getMSpline());
+        tester.startTest(TESTMODE_BENCHSET,"C.\temp_bleResult.csv");
+
+        Correct correct = new Correct(2, 2, 9, 9, phoneList,tester.getMSpline());
         correct.run();
-
-
-    }
-    
-    private static ArrayList fillList(){
-       ArrayList tmp  = new ArrayList();
-       // 梨꾩썙�빞�븿
-
-       return tmp;
     }
 }
